afea1b569a51b6c9ad071df2365e46cb8d054fb4
schochastics <david@schochastics.net>
2024-08-18 06:17:33 +0200

    added validator #1

--- R/utils.R
+++ R/utils.R
@@ -23,6 +23,17 @@ is.uuid <- function(x) {
 #' @param ... additional arguments to print
 #' @export
 as.uuid <- function(x) {
+    if (!validate.uuid(x)) {
+        stop("x is not a proper uuid")
+    }
     class(x) <- c("uuid", class(x))
     x
+}
+
+#' validate if string is uuid
+#' @param x a character
+#' @export
+validate.uuid <- function(x) {
+    rege <- "^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$"
+    grepl(rege, x)
 }
